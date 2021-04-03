e8eb540456d718a41d9f65296c192979294feb44
Ayush Ranjan <rajniayush1111@gmail.com>
2021-04-03 13:14:40 +0530

    initial commit completed upto caraousal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import  MyNavbar  from "./components/my-navbar/mynavbar.component";
+import MyCarousal from "./components/my-carousal/mycarousal.components";
+import TitleMessage from './components/title-message/titlemess.components';
+import MyScroll from './components/scroll-down/scrolldown.components';
 import './App.css';
 
-function App() {
+const App=() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyNavbar />
+      <MyCarousal />
+      <TitleMessage />
+      <MyScroll />
     </div>
   );
 }
